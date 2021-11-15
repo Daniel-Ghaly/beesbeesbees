b0f8289f65c4a28cccf08602898737461f8ef24f
Daniel-Ghaly <ghalydan@gmail.com>
2021-11-15 11:46:45 -0800

    Complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,17 @@
+// class Bee extends Grub {
+//   this.age = 5;
+//   super(food);
+
+// }
+
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
 
